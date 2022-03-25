394e6bdae80daa3675c8cd7ea3e0f18844016881
Ravi <ravikumr0403@gmail.com>
2022-03-25 15:18:05 +0530

    chore: change class to className

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,23 +3,23 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 
 export const VideoCard = () => {
   return (
-    <div class="card">
-      <div class="card-media">
+    <div className="card">
+      <div className="card-media">
         <div className="play-icon">
           <img src={playIcon} alt="" />
         </div>
         <img
-          class="img-responsive img-rounded"
+          className="img-responsive img-rounded"
           src="https://picsum.photos/800/400"
           alt="card media"
         />
       </div>
-      <div class="card-body">
-        <p class="card-title fs-1 m-0">Card Title Card Title</p>
+      <div className="card-body">
+        <p className="card-title fs-1 m-0">Card Title Card Title</p>
       </div>
-      <div class="card-footer">
+      <div className="card-footer">
         <div style={{ display: "flex", gap: "0.5rem" }}>
-          <div class="avatar avatar-xs">AB</div>
+          <div className="avatar avatar-xs">AB</div>
           <div>
             <p className="text-muted">1k views</p>
           </div>
